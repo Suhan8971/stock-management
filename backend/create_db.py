6c87d26977b551835ebacb08d496ee9fa4678edc
Suhan8971 <suhankaminofficial@gmail.com>
2026-05-20 09:37:17 +0530

    Credentials Restore

--- backend/create_db.py
+++ backend/create_db.py
@@ -1,7 +1,12 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 try:
-    conn = psycopg2.connect(user='postgres', password='123', host='localhost', port='5432')
+    conn = psycopg2.connect(user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'))
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cursor = conn.cursor()
     cursor.execute('CREATE DATABASE "CSD_Db"')
