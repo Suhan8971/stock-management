eda3d22f4399428d39c09bf76f5018b4a94a9468
Suhan8971 <suhankaminofficial@gmail.com>
2026-05-20 09:40:47 +0530

    Credentials Restore

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'CSD_Db'),
         'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', '123'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
