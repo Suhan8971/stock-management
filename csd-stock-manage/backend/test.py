c38dc57743b3d03e7e96b6e2ce4b43d75f2c8b89
Suhan8971 <suhankaminofficial@gmail.com>
2026-05-20 09:27:59 +0530

    Corrected Structure

--- csd-stock-manage/backend/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-django.setup()
-from django.db import connection
-cursor = connection.cursor()
-cursor.execute("SELECT column_name, is_nullable FROM information_schema.columns WHERE table_name = 'transactions';")
-for row in cursor.fetchall():
-    print(row)
